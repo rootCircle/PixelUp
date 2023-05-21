3ddf44855b44819de995c1e8636d44a0994ade3a
Lab Rat <dev.frolics@gmail.com>
2023-05-21 17:16:01 +0530

    Passing mutable reference of Image to library
    Using this might help in applying nested filters or so.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use pixel_up::Args;
 
-
 fn main() {
     let mut args = Args::new();
-    
-    let img_result = args.check_and_open();
-    
-    let img_result = img_result.unwrap();
 
-    args.apply_filters(img_result);
-    
+    let img_result = args.check_and_open();
+
+    let mut img_result = img_result.unwrap();
+
+    args.apply_filters(&mut img_result);
 }
