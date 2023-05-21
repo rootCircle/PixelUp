fef4334abcd2b54bb47b9e95b36c670f09922f5e
Lab Rat <dev.frolics@gmail.com>
2023-05-21 17:16:59 +0530

    More User Friendly output on Image save!

--- src/utils/image_util.rs
+++ src/utils/image_util.rs
@@ -2,9 +2,16 @@ use colored::Colorize;
 use image::DynamicImage;
 use std::path::PathBuf;
 
-pub fn save_image(img: DynamicImage, output_file: PathBuf) -> bool {
+pub fn save_image(img: &DynamicImage, output_file: &PathBuf) -> bool {
     match img.save(output_file) {
-        Ok(_) => true,
+        Ok(_) => {
+            println!(
+                "{} at {}",
+                "Image saved successfully".green().bold(),
+                output_file.to_string_lossy().bright_cyan()
+            );
+            true
+        }
         Err(err) => {
             eprintln!("{}", "Some Error Occured Saving file!".red());
             eprintln!("{}", err);
