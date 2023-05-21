a0c2a2ae2669bc6a441866d6bcf0cca09bbfedb8
Lab Rat <dev.frolics@gmail.com>
2023-05-21 17:20:10 +0530

    Added Negative Filter Support

--- src/photo_filters/img_negative.rs
+++ src/photo_filters/img_negative.rs
@@ -1,1 +1,17 @@
-pub fn apply_negative() {todo!()}
+use image::DynamicImage;
+
+pub fn apply_negative(img: &mut DynamicImage) -> DynamicImage {
+    let mut img = img.to_rgb8();
+    let (width, height) = img.dimensions();
+
+    for x in 0..width {
+        for y in 0..height {
+            let pixel = img.get_pixel(x, y);
+            let (red, green, blue) = (255 - pixel[0], 255 - pixel[1], 255 - pixel[2]);
+
+            img.put_pixel(x, y, image::Rgb([red, green, blue]));
+        }
+    }
+
+    DynamicImage::ImageRgb8(img)
+}
