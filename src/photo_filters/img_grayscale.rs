82aafe0f7ca6d5a9f89024703d588010147cc00b
Lab Rat <dev.frolics@gmail.com>
2023-05-21 17:18:34 +0530

    Better variable names, to increase code readability

--- src/photo_filters/img_grayscale.rs
+++ src/photo_filters/img_grayscale.rs
@@ -1,21 +1,20 @@
 use image::DynamicImage;
 
-
-pub fn apply_grayscale(img: &mut DynamicImage) -> DynamicImage{
+pub fn apply_grayscale(img: &mut DynamicImage) -> DynamicImage {
     let mut img = img.to_rgb8();
     let (width, height) = img.dimensions();
+
     for x in 0..width {
         for y in 0..height {
             let pixel = img.get_pixel(x, y);
-            let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
-            let mut avg = (r as u32 + g as u32 + b as u32) / 3;
-            
+            let (red, green, blue) = (pixel[0], pixel[1], pixel[2]);
+            let mut avg = (red as u32 + green as u32 + blue as u32) / 3;
+
             if avg > 255 {
                 avg = 255;
             }
 
             let avg = avg as u8;
-
             img.put_pixel(x, y, image::Rgb([avg, avg, avg]));
         }
     }
--- src/photo_filters/img_sepia.rs
+++ src/photo_filters/img_sepia.rs
@@ -3,14 +3,20 @@ use image::DynamicImage;
 pub fn apply_sepia_filter(img: &mut DynamicImage) -> DynamicImage {
     let mut img = img.to_rgb8();
     let (width, height) = img.dimensions();
+
     for x in 0..width {
         for y in 0..height {
             let pixel = img.get_pixel(x, y);
-            let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
-            let r_out = ((r as f32 * 0.393) + (g as f32 * 0.769) + (b as f32 * 0.189)) as u8;
-            let g_out = ((r as f32 * 0.349) + (g as f32 * 0.686) + (b as f32 * 0.168)) as u8;
-            let b_out = ((r as f32 * 0.272) + (g as f32 * 0.534) + (b as f32 * 0.131)) as u8;
-            img.put_pixel(x, y, image::Rgb([r_out, g_out, b_out]));
+            let (red, green, blue) = (pixel[0], pixel[1], pixel[2]);
+
+            let red_out =
+                ((red as f32 * 0.393) + (green as f32 * 0.769) + (blue as f32 * 0.189)) as u8;
+            let green_out =
+                ((red as f32 * 0.349) + (green as f32 * 0.686) + (blue as f32 * 0.168)) as u8;
+            let blue_out =
+                ((red as f32 * 0.272) + (green as f32 * 0.534) + (blue as f32 * 0.131)) as u8;
+
+            img.put_pixel(x, y, image::Rgb([red_out, green_out, blue_out]));
         }
     }
 
