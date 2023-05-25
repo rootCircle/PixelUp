486721e53496588976e1b54864b9917ba9cf2de3
Lab Rat <dev.frolics@gmail.com>
2023-05-25 15:08:11 +0530

    Some minor refractoring

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use pixel_up::Args;
 fn main() {
     let mut args = Args::new();
 
-    let img_result = args.check_and_open();
-
-    let mut img_result = img_result.unwrap();
+    let mut img_result = args.check_and_open().unwrap();
 
     args.apply_filters(&mut img_result);
 }
